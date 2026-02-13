5df930cd0e314e329e8ab8c8f5d77621f82e247e
APaud3l <aamod.paudel@coderacademy.edu.au>
2026-02-14 10:14:05 +1100

    feat: Initialised the server, wrote standard code for mongoDB connection and default GET / route

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/testdb';
+
+mongoose.connect(MONGO_URL)
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error: ', err));
+
+app.get('/', (request, response) => {
+    response.json(
+        {
+            "message": "Express and Mongo Server is running!!"
+        }
+    )
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+});
