a80da7355c6896ac9552da46a84bf7ee624d2bf9
APaud3l <aamod.paudel@coderacademy.edu.au>
2026-02-14 11:49:26 +1100

    Added Dockerfile and docker-compose

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/testdb';
 
 mongoose.connect(MONGO_URL)
-.then(() => console.log('MongoDB connected!'))
+.then(() => console.log('MongoDB connected successfully!'))
 .catch(err => console.error('MongoDB connection error: ', err));
 
 app.get('/', (request, response) => {
